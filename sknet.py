53f60b642e60fbccc331c2ce88e2cd16aa842142
pengcheng93 <32193193+pengcheng93@users.noreply.github.com>
2019-07-05 11:04:11 +0800

    Add files via upload

--- sknet.py
+++ sknet.py
@@ -1,6 +1,83 @@
 import torch
 from torch import nn
 
+bn_momentum=0.1
+class SKConv1 (nn.Module):
+    def __init__(self, features,out_channel, M, G, r, stride=1, L=32):
+        super (SKConv1, self).__init__ ()
+        d = max (int (features / r), L)
+        self.M = M
+        self.features = features
+        self.outchannel=out_channel
+
+        self.conv1=nn.Sequential (
+            nn.Conv2d (features, out_channel, kernel_size=3, stride=stride, padding=1,groups=G,bias=False),
+            nn.BatchNorm2d (out_channel,momentum=bn_momentum),
+            nn.ReLU (inplace=True)
+        )
+        self.conv2=nn.Sequential (
+            nn.Conv2d (features, out_channel, kernel_size=3, stride=stride, padding=2,groups=G,bias=False,dilation=2),
+            nn.BatchNorm2d (out_channel,momentum=bn_momentum),
+            nn.ReLU (inplace=True)
+        )
+        # self.gap = nn.AvgPool2d (int(WH/stride))
+        self.gap=nn.AdaptiveAvgPool2d(1)
+        self.fc1 = nn.Sequential (
+                nn.Conv2d (out_channel, d, 1, padding=0,bias=False),
+                # nn.BatchNorm2d (d),
+                nn.ReLU (inplace=True)
+            )
+        self.fc2=nn.Sequential (
+                nn.Conv2d (d, out_channel*2, 1, padding=0,bias=False),
+                # nn.BatchNorm2d (256),
+                nn.ReLU (inplace=True)
+            )
+        self.softmax = nn.Softmax (dim=1)
+
+    def forward(self, x):
+        fea1=self.conv1(x)
+        fea2=self.conv2(x)
+        fea_U = fea1+fea2
+        fea_s = self.gap (fea_U)
+        fea_z = self.fc1 (fea_s)
+        fea_z=self.fc2(fea_z)
+        fea_z=fea_z.view(fea_z.shape[0],2,-1,fea_z.shape[-1])
+
+
+        attention_vectors = self.softmax (fea_z)
+        attention_vectors1,attention_vectors2=torch.split(attention_vectors,1,dim=1)
+
+        attention_vectors1=attention_vectors1.reshape(attention_vectors1.shape[0],self.outchannel,-1,attention_vectors1.shape[-1])
+        attention_vectors2=attention_vectors2.reshape(attention_vectors2.shape[0],self.outchannel,-1,attention_vectors2.shape[-1])
+        out1 = attention_vectors1*fea1
+        out2 = attention_vectors2*fea2
+        out=out1+out2
+        return out
+
+class SKConv2(nn.Module):
+    def __init__(self, channel, reduction):
+        super(SKConv2, self).__init__()
+        self.conv1 = nn.Conv2d(channel, channel, 3, padding=1, bias=True)
+        self.conv2 = nn.Conv2d(channel, channel, 3, padding=2, dilation=2, bias=True)
+        self.pool = nn.AdaptiveAvgPool2d(1)
+        self.conv_se = nn.Sequential(
+            nn.Conv2d(channel, channel//reduction, 1, padding=0, bias=True),
+            nn.ReLU(inplace=True)
+        )
+        self.conv_ex = nn.Sequential(nn.Conv2d(channel//reduction, channel, 1, padding=0, bias=True))
+        self.softmax = nn.Softmax(dim=1)
+    def forward(self, x):
+        conv1 = self.conv1(x).unsqueeze(dim=1)
+        conv2 = self.conv2(x).unsqueeze(dim=1)
+        features = torch.cat([conv1, conv2], dim=1)
+        U = torch.sum(features, dim=1)
+        S = self.pool(U)
+        Z = self.conv_se(S)
+        attention_vector = torch.cat([self.conv_ex(Z).unsqueeze(dim=1), self.conv_ex(Z).unsqueeze(dim=1)], dim=1)
+        attention_vector = self.softmax(attention_vector)
+        V = (features * attention_vector).sum(dim=1)
+        return V
+
 
 class SKConv (nn.Module):
     def __init__(self, features, WH, M, G, r, stride=1, L=32):
@@ -113,11 +190,12 @@ class SKUnit (nn.Module):
 
 
 if __name__ == '__main__':
-    dump_input = torch.rand ((2,
-                              64,
+    dump_input = torch.rand ((64,
                               128,
-                              128))
+                              64,
+                              64))
 
-    sk=SKConv(64,128,2,8,2)
+    sk=SKConv(128,128,2,8,2)
+    # sk=SKAttention(64,2)
     res=sk(dump_input)
     print(res.shape)
